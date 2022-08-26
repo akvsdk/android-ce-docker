de8a61f6be23399fa19c5a062b78197729942459
j1ang <dkvsak@gmail.com>
2022-08-26 08:04:37 +0800

    fix spider file

--- init.sh
+++ init.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
-
-git checkout .
-
-git pull
-
-git rev-parse --short HEAD
-
-echo pip -V
-
 #sed  -i 's/timeout=5/timeout=os.environ.get(\"SEARCH_TIME\")/' app.py
 
 #sed  -i 's/ali = "" \"\"/import os \nali = os.environ.get(\"REFRESH_TOKEN\")/' utils/ali.py
 
-gunicorn -w 4 -b 0.0.0.0:8080 app:app
+BRANCH=main
+
+LOCAL=$(git log $BRANCH -n 1 --pretty=format:"%H")
+REMOTE=$(git log remotes/origin/$BRANCH -n 1 --pretty=format:"%H")
+
+if [ $LOCAL = $REMOTE ]; then
+
+    git checkout . && git pull
+
+    pip install -i https://pypi.mirrors.ustc.edu.cn/simple/ -r requirements.txt && gunicorn -w 4 -b 0.0.0.0:8080 app:app
+
+else
+    gunicorn -w 4 -b 0.0.0.0:8080 app:app
+fi
